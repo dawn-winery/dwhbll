e02111302e4b842372b3c5d373ca2b6c6b8084a6
Johan DANIEL <johan-daniel-perso@proton.me>
2025-06-12 23:32:26 +0200

    Optimised matrix multiplication

--- include/dwhbll/linalg/matrix.h
+++ include/dwhbll/linalg/matrix.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <array>
+#include <thread>
+#include <vector>
 #include <cassert>
 #include <concepts>
 #include <cstddef>
@@ -10,18 +11,16 @@ namespace dwhbll::linealg {
 template <typename T, size_t Row, size_t Col> requires std::default_initializable<T>
 class Matrix {
     private:
-        std::array<T, Row * Col> data;
+        std::vector<T> data;
     
     public:
-        Matrix() : data{}
+        Matrix() : data(Row * Col)
         {
         }
 
         template<typename... Args> requires (sizeof...(Args) == Row * Col && (std::convertible_to<Args, T> && ...))
-        Matrix(Args... d)
+        Matrix(Args&&... d) : data { static_cast<T>(std::forward<Args>(d))... }
         {
-            size_t i = 0;
-            ((data[i++] = static_cast<T>(d)), ...);
         }
 
         constexpr size_t rowCount() const { return Row; }
@@ -35,17 +34,37 @@ class Matrix {
         }
 
         template<size_t K>
-        Matrix<T, Row, K> operator*(const Matrix<T, Col, K>& other)
+        Matrix<T, Row, K> operator*(Matrix<T, Col, K>& other)
         {
             Matrix<T, Row, K> res;
 
-            for(size_t row = 0; row < Row; row++) {
-                for(size_t col = 0; col < K; col++) {
+            auto MatmulWorker = [&] (size_t start_row, size_t end_row) {
+                for(size_t row = start_row; row < end_row; row++) {
                     for(size_t cur = 0; cur < Col; cur++) {
-                        res[row][col] += data[row][cur] * other[cur][col];
+                        for(size_t col = 0; col < K; col++) {
+                            res[row, col] += (*this)[row, cur] * other[cur, col];
+                        }
                     }
                 }
+            };
+
+            size_t num_threads = std::thread::hardware_concurrency();
+            size_t row_per_threads = Row / num_threads;
+            size_t remaining_rows  = Row % num_threads;
+
+            std::vector<std::jthread> threads;
+            
+            for(int i = 0; i < num_threads; i++) {
+                size_t start_row = i * row_per_threads;
+                size_t end_row = start_row + row_per_threads;
+
+                if (i == num_threads - 1) {
+                    end_row += remaining_rows;
+                }
+
+                threads.emplace_back(MatmulWorker, start_row, end_row);
             }
+
 
             return res;
         }
--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -1,7 +1,10 @@
+#include <chrono>
 #include <dwhbll/linalg/matrix.h>
 #include <functional>
 #include <initializer_list>
 #include <iostream>
+#include <print>
+#include <random>
 #include <string>
 #include <optional>
 #include <unordered_map>
@@ -17,7 +20,7 @@ bool matrix_init()
 
         dwhbll::linealg::Matrix<int, M, N> mat;
         test_failed |= mat.storageSize() != (M * N);
-        if(test_failed) std::println(std::cerr, "Failed to allocated enough storage");
+        if(test_failed) std::println(std::cerr, "[Zero init] Failed to allocated enough storage");
 
         for(int i = 0; i < M; i++) {
             for(int j = 0; j < M; j++) {
@@ -26,7 +29,7 @@ bool matrix_init()
         }
 
         if(test_failed)
-            std::println(std::cerr, "Zero initialization failed");
+            std::println(std::cerr, "[Zero init] Initialization failed");
     }
 
     // Test init list
@@ -39,7 +42,7 @@ bool matrix_init()
 
         test_failed |= mat.storageSize() != 9;
         if(test_failed)
-            std::println(std::cerr, "Failed to allocated enough storage");
+            std::println(std::cerr, "[Init list] Failed to allocated enough storage");
 
 
         for(int i = 0; i < 3; i++) {
@@ -49,7 +52,7 @@ bool matrix_init()
         }
 
         if(test_failed)
-            std::println(std::cerr, "Initialization list failed");
+            std::println(std::cerr, "[Init list] Initialization failed");
     }
 
     return test_failed;
@@ -57,12 +60,77 @@ bool matrix_init()
 
 bool matrix_mul() {
 
+    bool test_failed = false;
 
-    return false;
+    // Square matrix
+    {
+        dwhbll::linealg::Matrix<int, 4, 4> m1 {
+            1, 2, 3, 4,
+            5, 6, 7, 8,
+            9, 10, 11, 12,
+            13, 14, 15, 16
+        };
+
+        dwhbll::linealg::Matrix<int, 4, 4> m2 {
+            16, 15, 14, 13,
+            12, 11, 10, 9,
+            8, 7, 6, 5,
+            4, 3, 2, 1
+        };
+
+        dwhbll::linealg::Matrix<int, 4, 4> expected {
+            80, 70, 60, 50,
+            240, 214, 188, 162,
+            400, 358, 316, 274,
+            560, 502, 444, 386
+        };
+
+        auto res = m1 * m2;
+        test_failed |= (res.rowCount() != m1.rowCount() || res.colCount() != m2.colCount());
+        if(test_failed) 
+            std::println(std::cerr, "[Square] Result matrix of wrong dimensions. Expected {}x{}, got {}x{}", 
+                m1.rowCount(), m2.colCount(), 
+                res.rowCount(), res.colCount()
+            );
+
+        for(int i = 0; i < res.rowCount(); i++) {
+            for(int j = 0; j < res.colCount(); j++) {
+                bool failed = (res[i,j] != expected[i,j]);
+                test_failed |= failed;
+                if(failed)
+                    std::println(std::cerr, "[Square] Multiplication failed at [{},{}]. Expected {}, got {}", i, j, expected[i,j], res[i,j]);
+            }
+        }
+    }
+
+    {
+        constexpr size_t SIZE = 8192;
+        dwhbll::linealg::Matrix<int, SIZE, SIZE> m1;
+        dwhbll::linealg::Matrix<int, SIZE, SIZE> m2;
+
+        std::random_device rd;
+        std::mt19937_64 generator(rd());
+        std::uniform_int_distribution<int> distrib(0, 128);
+
+        for(auto i = 0; i < SIZE; i++) {
+            for(auto j = 0; j < SIZE; j++) {
+                m1[i,j] = distrib(generator);
+                m2[i,j] = distrib(generator); 
+            }
+        }
+
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto res = m1 * m2;
+        auto t2 = std::chrono::high_resolution_clock::now();
+        std::println(std::cout, "Mat mul of matrices {}x{} took {}", SIZE, SIZE, std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1));
+    }
+
+    return test_failed;
 }
 
 std::unordered_map<std::string, std::function<bool()>> test_dispatch {
     { "init", matrix_init },
+    { "mul", matrix_mul }
 };
 
 bool matrix_test(std::optional<std::string> test_to_run) 
