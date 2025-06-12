885c51dd6e9beb25a4431cc529f7683f50272d09
danebidev <57720273+danebidev@users.noreply.github.com>
2025-06-12 15:37:32 +0200

    Fix johan's naming

--- include/dwhbll/linalg/matrix.h
+++ include/dwhbll/linalg/matrix.h
@@ -7,40 +7,40 @@
 
 namespace dwhbll::linealg {
 
-template <typename T, size_t M, size_t N> requires std::default_initializable<T>
+template <typename T, size_t Row, size_t Col> requires std::default_initializable<T>
 class Matrix {
     private:
-        std::array<T, M * N> data;
+        std::array<T, Row * Col> data;
     
     public:
         Matrix() : data{}
         {
         }
 
-        template<typename... Args> requires (sizeof...(Args) == M * N && (std::convertible_to<Args, T> && ...))
+        template<typename... Args> requires (sizeof...(Args) == Row * Col && (std::convertible_to<Args, T> && ...))
         Matrix(Args... d)
         {
             size_t i = 0;
             ((data[i++] = static_cast<T>(d)), ...);
         }
 
-        constexpr size_t rowCount() const { return M; }
-        constexpr size_t colCount() const { return N; }
+        constexpr size_t rowCount() const { return Row; }
+        constexpr size_t colCount() const { return Col; }
         constexpr size_t storageSize() const { return data.size(); }
 
-        T& operator[](size_t i, size_t j)
+        T& operator[](size_t row, size_t col)
         {
-            assert(i < M && j < N);
-            return data[i * M + j];
+            assert(row < Row && col < Col);
+            return data[row * Col + col];
         }
 
         template<size_t K>
-        Matrix<T, M, K> operator*(const Matrix<T, N, K>& other)
+        Matrix<T, Row, K> operator*(const Matrix<T, Col, K>& other)
         {
-            Matrix<T, M, K> res;
+            Matrix<T, Row, K> res;
 
-            for(size_t i = 0; i < M; i++) {
-                for(size_t j = 0; j < N; i++) {
+            for(size_t i = 0; i < Row; i++) {
+                for(size_t j = 0; j < Col; i++) {
                     for(size_t k = 0; k < 0; k++) {
                         res[i][k] += data[i][j] * other[j][k];
                     }
