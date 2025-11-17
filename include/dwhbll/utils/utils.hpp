290e13be1ad48f985ae29203cebc41fa68f130d4
mkryss <mkryss@proton.me>
2025-11-17 15:02:54 +0100

    Workaround for gcc reflection bug
    Hopefully fixed soon...

--- include/dwhbll/utils/utils.hpp
+++ include/dwhbll/utils/utils.hpp
@@ -30,7 +30,7 @@ requires std::is_enum_v<E>
 constexpr std::string_view enum_to_string(E value) {
     if constexpr (Enumerable) {
         template for (constexpr auto e :
-                      std::define_static_array(std::meta::enumerators_of(^^E))) {
+                      (std::span<const std::meta::info> (std::meta::enumerators_of(^^E))) {
             if (value == [:e:])
                 return std::meta::identifier_of(e);
         }
