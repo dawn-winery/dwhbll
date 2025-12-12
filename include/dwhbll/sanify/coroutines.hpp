cd6c22e12ba501e669bef1c5dd92fad0b3e8e4a2
teapot <teapot4195@gmail.com>
2025-12-12 17:13:53 -0500

    Add the new types to sanify and a using namespace dwhbll::concurrency::coroutines;

--- include/dwhbll/sanify/coroutines.hpp
+++ include/dwhbll/sanify/coroutines.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <dwhbll/concurrency/coroutine/task.h>
+#include <dwhbll/concurrency/coroutine/sleep_task.h>
+#include <dwhbll/concurrency/coroutine/reactor.h>
+
 #define yield co_yield
 #define await co_await
 #define finish co_return
+
+using namespace dwhbll::concurrency::coroutine;
