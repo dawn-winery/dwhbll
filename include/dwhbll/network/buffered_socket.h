d7fedd44d6756cb02e5ea793a1b78586590270fe
teapot <teapot4195@gmail.com>
2025-12-14 16:16:18 -0500

    Add async to buffered socket

--- include/dwhbll/network/buffered_socket.h
+++ include/dwhbll/network/buffered_socket.h
@@ -12,6 +12,8 @@ namespace dwhbll::network {
         inbound_network_buffer(memory::Pool<Socket>::ObjectWrapper& socket);
 
         void refill_buffer() override;
+
+        concurrency::coroutine::task<> refill_buffer_async() override;
     };
 
     class outbound_network_buffer : public files::ParseUtils {
@@ -21,6 +23,8 @@ namespace dwhbll::network {
         outbound_network_buffer(memory::Pool<Socket>::ObjectWrapper& socket);
 
         void flush();
+
+        concurrency::coroutine::task<> flush_async();
     };
 
     class buffered_socket {
@@ -30,9 +34,21 @@ namespace dwhbll::network {
         inbound_network_buffer inbound;
         outbound_network_buffer outbound;
 
+        buffered_socket();
+
         buffered_socket(memory::Pool<Socket>::ObjectWrapper &&socket);
 
+        buffered_socket(const buffered_socket &other) = delete;
+
+        buffered_socket(buffered_socket &&other) noexcept;
+
+        buffered_socket & operator=(const buffered_socket &other) = delete;
+
+        buffered_socket & operator=(buffered_socket &&other) noexcept = delete;
+
         void flush_outbound();
+
+        concurrency::coroutine::task<> flush_outbound_async();
 
         memory::Pool<Socket>::ObjectWrapper& socket_ref();
     };
--- src/dwhbll/network/buffered_socket.cpp
+++ src/dwhbll/network/buffered_socket.cpp
@@ -1,5 +1,9 @@
 #include "../../../include/dwhbll/network/buffered_socket.h"
 
+#include <dwhbll/console/debug.hpp>
+#include <dwhbll/exceptions/rt_exception_base.h>
+#include <dwhbll/sanify/coroutines.hpp>
+
 namespace dwhbll::network {
     inbound_network_buffer::inbound_network_buffer(memory::Pool<Socket>::ObjectWrapper &socket) : socket(socket), ParseUtils() {}
 
@@ -7,14 +11,31 @@ namespace dwhbll::network {
         // TODO: zero copy this part in the future
         std::vector<char> buffer(1024);
 
-        auto recv_count = socket->recv(buffer);
+        auto recv_count = socket->read(buffer);
 
         // TODO: what is this lmao
-        if (recv_count == -1 || recv_count == 0)
+        if ((recv_count == -1 && (errno != EAGAIN && errno != EWOULDBLOCK)) || recv_count == 0)
+            return;
+
+        if (recv_count == -1 && (errno == EAGAIN || errno == EWOULDBLOCK))
             return;
 
         buffer.resize(recv_count);
         write_vector(std::span{*reinterpret_cast<std::vector<sanify::u8> *>(&buffer)});
+    }
+
+    task<> inbound_network_buffer::refill_buffer_async() {
+        // TODO: zero copy this part in the future
+        std::vector<char> buffer(1024);
+
+        auto recv_count = await socket->read_async(buffer);
+
+        // TODO: what is this lmao
+        if ((recv_count == -1 && (errno != EAGAIN && errno != EWOULDBLOCK)) || recv_count == 0)
+            co_return;
+
+        if (recv_count == -1 && (errno == EAGAIN || errno == EWOULDBLOCK))
+            co_return;
     }
 
     outbound_network_buffer::outbound_network_buffer(memory::Pool<Socket>::ObjectWrapper &socket) : socket(socket) {}
@@ -27,10 +48,33 @@ namespace dwhbll::network {
         buffer.clear();
     }
 
-    buffered_socket::buffered_socket(memory::Pool<Socket>::ObjectWrapper &&socket) : socket(std::move(socket)), inbound(this->socket), outbound(this->socket) {}
+    task<> outbound_network_buffer::flush_async() {
+        buffer.make_cont();
+
+        await socket->send_async(std::span{reinterpret_cast<char *>(buffer.data().data()), buffer.size()});
+
+        buffer.clear();
+    }
+
+    buffered_socket::buffered_socket() : socket(nullptr, nullptr), inbound(this->socket), outbound(this->socket) {}
+
+    buffered_socket::buffered_socket(memory::Pool<Socket>::ObjectWrapper &&socket) : socket(std::move(socket)), inbound(this->socket), outbound(this->socket) {
+    }
+
+    buffered_socket::buffered_socket(buffered_socket &&other) noexcept: socket(std::move(other.socket)),
+                                                                        inbound(this->socket),
+                                                                        outbound(this->socket) {
+        // TODO: fix this by using std::shared_ptr.
+        if (!other.inbound.empty() || !other.outbound.empty())
+            debug::panic("buffered_socket was moved with data inside the buffers! this is not allowed!");
+    }
 
     void buffered_socket::flush_outbound() {
         outbound.flush();
+    }
+
+    task<> buffered_socket::flush_outbound_async() {
+        return outbound.flush_async();
     }
 
     memory::Pool<Socket>::ObjectWrapper & buffered_socket::socket_ref() {
