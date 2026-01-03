608eb4b14d131695a1b9f3a3c47cea772c9108e4
teapot <teapot4195@gmail.com>
2026-01-03 17:25:37 -0500

    Reformated bitmap

--- include/dwhbll/graphics/bitmap/bitmap.h
+++ include/dwhbll/graphics/bitmap/bitmap.h
@@ -1,18 +1,21 @@
 #pragma once
 
-#include <stdint.h>
+#include <cstdint>
 #include <string>
 
 namespace dwhbll::graphics::bitmap {
-class bitmap {
-  int width;
-  int height;
-  uint8_t *pixels;
+    class bitmap {
+        int width;
+        int height;
+        uint8_t *pixels;
 
-public:
-  bitmap(int width, int height);
-  ~bitmap();
-  bool set_pixel(int x, int y, uint8_t r, uint8_t g, uint8_t b);
-  bool write_to_file(const std::string &filename);
-};
+    public:
+        bitmap(int width, int height);
+
+        ~bitmap();
+
+        [[nodiscard]] bool set_pixel(int x, int y, uint8_t r, uint8_t g, uint8_t b) const;
+
+        [[nodiscard]] bool write_to_file(const std::string &filename) const;
+    };
 } // namespace dwhbll::graphics::bitmap
