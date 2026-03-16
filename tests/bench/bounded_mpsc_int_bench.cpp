98636f3435a73bdd16203eeb78a398970eb526d2
teapot <teapot4195@gmail.com>
2026-03-16 16:30:55 -0400

    Benchmark improvements (use Exponential backoff policy)

--- tests/bench/bounded_mpsc_int_bench.cpp
+++ tests/bench/bounded_mpsc_int_bench.cpp
@@ -1,51 +1,72 @@
 #include <chrono>
+#include <latch>
 #include <optional>
 #include <string>
 #include <thread>
 
+#include <dwhbll/concurrency/backoff/policy_exponential.h>
 #include <dwhbll/concurrency/queues/bounded_mpsc_queue.h>
+#include <dwhbll/console/debug.hpp>
 #include <dwhbll/console/Logging.h>
+#include <dwhbll/utils/threading.h>
 
 // TODO: Make a benchmark harness and do this correctly!
 bool bounded_mpsc_int_bench(std::optional<std::string> _) {
-    dwhbll::concurrency::queues::BoundedMPSCQueue<std::size_t, 8192> channel;
+    constexpr std::size_t count = 50000000;
+
+    dwhbll::concurrency::queues::BoundedMPSCQueue<std::size_t, 8192, false, dwhbll::concurrency::backoff::PolicyExponential> channel;
+
+    const auto thread_count = std::thread::hardware_concurrency();
+
+    const auto producer_count = std::max(thread_count / 2 - 1, 1u);
+
+    std::latch waiter(producer_count + 1);
 
     std::vector<std::thread> threads;
 
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < producer_count; i++) {
         threads.emplace_back([&] {
-            std::size_t x = 0;
+            std::size_t x = 1;
 
-            while (x < 50000000) {
+            // dwhbll::utils::pin_thread_to_core(i * 2);
+
+            waiter.arrive_and_wait();
+
+            while (x <= count) {
                 if (channel.put(x))
                     x++;
             }
-
-            channel.put(0xFFFFFFFFFFFFFFFF);
         });
     }
 
+    // dwhbll::utils::pin_thread_to_core(producer_count);
+
+    waiter.arrive_and_wait();
+
     using namespace std::chrono_literals;
-    auto start = std::chrono::steady_clock::now();
+    const auto start = std::chrono::steady_clock::now();
+
+    std::size_t total = 0;
 
     std::size_t x = 0;
 
-    std::size_t done = 0;
-
     while (true) {
         if (const auto next = channel.get(); next.has_value()) {
-            if (++x == 50000000 * 8)
-            // if (next.value() == 0xFFFFFFFFFFFFFFFF && ++done == 8)
+            total += next.value();
+            if (++x == count * producer_count)
                 break;
         }
     }
 
-    auto now = std::chrono::steady_clock::now();
+    const auto now = std::chrono::steady_clock::now();
+
+    if (total != count * (count + 1) / 2 * producer_count)
+        dwhbll::debug::panic("[MPSC Queue] Incorrect queue behavior! Total sum of all elements {} instead of {}", total, count * (count + 1) / 2 * producer_count);
 
     dwhbll::console::info("[MPSC Queue] Ran for {}, acquired {} messages, {} ops/msec",
       std::chrono::duration_cast<std::chrono::milliseconds>((now - start)),
       x,
-      (double)x / (double)std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count()
+      static_cast<double>(x) / static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count())
     );
 
     for (auto& thread : threads)
--- tests/bench/bounded_spsc_int_bench.cpp
+++ tests/bench/bounded_spsc_int_bench.cpp
@@ -1,40 +1,66 @@
 #include <chrono>
+#include <latch>
 #include <optional>
 #include <string>
 #include <thread>
 
+#include <dwhbll/concurrency/backoff/policy_exponential.h>
 #include <dwhbll/concurrency/queues/bounded_spsc_queue.h>
+#include <dwhbll/console/debug.hpp>
 #include <dwhbll/console/Logging.h>
+#include <dwhbll/utils/threading.h>
 
 // TODO: Make a benchmark harness and do this correctly!
 bool bounded_spsc_int_bench(std::optional<std::string> _) {
-    dwhbll::concurrency::queues::BoundedSPSCQueue<std::size_t, 8192> channel;
+    const std::size_t count = 2000000000;
+    dwhbll::concurrency::queues::BoundedSPSCQueue<std::size_t, 8192, false, dwhbll::concurrency::backoff::PolicyExponential> channel;
+
+    const auto threads = std::thread::hardware_concurrency();
+
+    std::latch waiter(2);
 
     std::thread publisher([&] {
-        std::size_t x = 0;
+        std::size_t x = 1;
 
-        while (x < 500000000) {
-            if (channel.put(x))
-                x++;
+        if (threads > 2)
+            dwhbll::utils::pin_thread_to_core(2);
+        else if (threads == 2)
+            dwhbll::utils::pin_thread_to_core(1);
+        else
+            dwhbll::utils::pin_thread_to_core(0);
+
+        waiter.arrive_and_wait();
+
+        while (x <= count) {
+            channel.put(x++);
         }
 
         channel.put(0xFFFFFFFFFFFFFFFF);
     });
 
+    dwhbll::utils::pin_thread_to_core(0);
+
+    waiter.arrive_and_wait();
+
     using namespace std::chrono_literals;
     auto start = std::chrono::steady_clock::now();
 
     std::size_t x = 0;
+    std::size_t total = 0;
 
     while (true) {
         if (const auto next = channel.get(); next.has_value()) {
-          if (next.value() == 0xFFFFFFFFFFFFFFFF)
-            break;
-          x++;
+            if (next.value() == 0xFFFFFFFFFFFFFFFF)
+                break;
+            x++;
+            total += next.value();
         }
     }
 
     auto now = std::chrono::steady_clock::now();
+
+    if (total != count * (count + 1) / 2)
+        dwhbll::debug::panic("[SPSC Queue] Total sum expected to be {}, got {}!", count * (count + 1) / 2, total);
 
     dwhbll::console::info("[SPSC Queue] Ran for {}, acquired {} messages, {} ops/msec",
       std::chrono::duration_cast<std::chrono::milliseconds>((now - start)),
