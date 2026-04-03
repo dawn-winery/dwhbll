8b832046e1565f2cf0cfd73e3d047ab6b2b65948
teapot4195 <teapot4195@gmail.com>
2026-04-03 14:41:01 -0400

    Add copy and move operations as well as equality.

--- include/dwhbll/utils/uuid.h
+++ include/dwhbll/utils/uuid.h
@@ -16,6 +16,18 @@ namespace dwhbll::utils {
 
         explicit UUID(const std::array<std::uint8_t, 16>& data);
 
+        UUID(const UUID &other);
+
+        UUID(UUID &&other) noexcept;
+
+        UUID & operator=(const UUID &other);
+
+        UUID & operator=(UUID &&other) noexcept;
+
         [[nodiscard]] std::string to_string() const;
+
+        friend bool operator==(const UUID &lhs, const UUID &rhs);
+
+        friend bool operator!=(const UUID &lhs, const UUID &rhs);
     };
 }
--- src/dwhbll/utils/uuid.cpp
+++ src/dwhbll/utils/uuid.cpp
@@ -11,9 +11,37 @@ namespace dwhbll::utils {
 
     UUID::UUID(const std::array<std::uint8_t, 16> &data) : _data(data) {}
 
+    UUID::UUID(const UUID &other): _data(other._data) {
+    }
+
+    UUID::UUID(UUID &&other) noexcept: _data(std::move(other._data)) {
+    }
+
+    UUID & UUID::operator=(const UUID &other) {
+        if (this == &other)
+            return *this;
+        _data = other._data;
+        return *this;
+    }
+
+    UUID & UUID::operator=(UUID &&other) noexcept {
+        if (this == &other)
+            return *this;
+        _data = std::move(other._data);
+        return *this;
+    }
+
     std::string UUID::to_string() const {
         return std::format("{:x}{:x}{:x}{:x}-{:x}{:x}-{:x}{:x}-{:x}{:x}-{:x}{:x}{:x}{:x}{:x}{:x}",
             _data[0], _data[1], _data[2], _data[3], _data[4], _data[5], _data[6], _data[7],
             _data[8], _data[9], _data[10], _data[11], _data[12], _data[13], _data[14], _data[15]);
+    }
+
+    bool operator==(const UUID &lhs, const UUID &rhs) {
+        return lhs._data == rhs._data;
+    }
+
+    bool operator!=(const UUID &lhs, const UUID &rhs) {
+        return !(lhs == rhs);
     }
 }
