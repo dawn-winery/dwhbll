9e9308b482ccd852895c31bc8e24896f0c83254d
Soham Nandy <soham.nandy2006@gmail.com>
2026-07-06 18:41:07 +0530

    feat(logging): add automatic debug formatting

--- include/dwhbll/console/logging.h
+++ include/dwhbll/console/logging.h
@@ -4,6 +4,9 @@
 #include <list>
 #include <string>
 #include <unordered_map>
+#include <dwhbll/utils/format.hpp>
+
+#include <type_traits>
 
 namespace dwhbll::console {
     enum class Level {
@@ -23,6 +26,56 @@ namespace dwhbll::console {
         extern Level defaultLevel;
         extern Level cerrLevel; ///< Level at which logs more important than this will be sent to stderr instead of stdout
         extern bool colors;
+
+        // bs incoming
+        
+        template <typename T>
+        decltype(auto) log_arg_to_format_arg(T&& value)
+        {
+            using U = std::remove_cvref_t<T>;
+     
+            if constexpr (std::formattable<U, char>) {
+                return std::forward<T>(value);
+            } 
+#if __cpp_impl_reflection >= 202506L                 
+            else {
+                return ::dwhbll::debug::dbg(value);
+            }   
+#else
+            else {
+                static_assert(
+                    false,
+                    "This type is not std::formattable. "
+                    "Compile this target with reflection support to log arbitrary structs."
+                );
+            }
+#endif
+        }
+
+        template <typename T>
+        using log_format_arg_t =
+            std::remove_cvref_t<decltype(log_arg_to_format_arg(std::declval<T>()))>;
+
+        template <typename... Args>
+        std::string render(
+            std::format_string<log_format_arg_t<Args>...> fmt,
+            Args&&... args
+        ) {
+            auto converted = std::tuple<log_format_arg_t<Args>...>{
+                log_arg_to_format_arg(std::forward<Args>(args))...
+            };
+        
+            return std::apply(
+                [&](auto&... values) {
+                    return std::vformat(
+                        fmt.get(),
+                        std::make_format_args(values...)
+                    );
+                },
+                converted
+            );
+        }
+        
     }
 
     void setLevel(Level level);
@@ -35,10 +88,11 @@ namespace dwhbll::console {
 
     template <typename... Args>
     requires (sizeof...(Args) != 0)
-    void log(const std::string& msg, const Level level, Args&&... args) {
+    void log(std::format_string<detail::log_format_arg_t<Args>...> fmt, 
+        const Level level, Args&&... args) {
         if (level < detail::defaultLevel)
             return;
-        log(std::vformat(msg, std::make_format_args(args...)), level);
+        log(detail::render(fmt, std::forward<Args>(args)...),level);
     }
 
     void fatal(const std::string& msg);
@@ -57,58 +111,65 @@ namespace dwhbll::console {
 
     template <typename... Args>
     requires (sizeof...(Args) != 0)
-    void fatal(const std::string& msg, Args&&... args) {
+    void fatal(std::format_string<detail::log_format_arg_t<Args>...> fmt,
+        Args&&... args) {
         if (Level::FATAL < detail::defaultLevel)
             return;
-        fatal(std::vformat(msg, std::make_format_args(args...)));
+        fatal(detail::render(fmt, std::forward<Args>(args)...));
     }
 
     template <typename... Args>
     requires (sizeof...(Args) != 0)
-    void critical(const std::string& msg, Args&&... args) {
+    void critical(std::format_string<detail::log_format_arg_t<Args>...> fmt, 
+        Args&&... args) {
         if (Level::CRITICAL < detail::defaultLevel)
             return;
-        critical(std::vformat(msg, std::make_format_args(args...)));
+        critical(detail::render(fmt, std::forward<Args>(args)...));
     }
 
     template <typename... Args>
     requires (sizeof...(Args) != 0)
-    void error(const std::string& msg, Args&&... args) {
+    void error(std::format_string<detail::log_format_arg_t<Args>...> fmt,
+        Args&&... args) {
         if (Level::ERROR < detail::defaultLevel)
             return;
-        error(std::vformat(msg, std::make_format_args(args...)));
+        error(detail::render(fmt, std::forward<Args>(args)...));
     }
 
     template <typename... Args>
     requires (sizeof...(Args) != 0)
-    void warn(const std::string& msg, Args&&... args) {
+    void warn(std::format_string<detail::log_format_arg_t<Args>...> fmt,
+        Args&&... args) {
         if (Level::WARN < detail::defaultLevel)
             return;
-        warn(std::vformat(msg, std::make_format_args(args...)));
+        warn(detail::render(fmt, std::forward<Args>(args)...));
     }
 
     template <typename... Args>
     requires (sizeof...(Args) != 0)
-    void info(const std::string& msg, Args&&... args) {
+    void info(std::format_string<detail::log_format_arg_t<Args>...> fmt,
+        Args&&... args) {
         if (Level::INFO < detail::defaultLevel)
             return;
-        info(std::vformat(msg, std::make_format_args(args...)));
+        info(detail::render(fmt, std::forward<Args>(args)...));
     }
 
     template <typename... Args>
     requires (sizeof...(Args) != 0)
-    void debug(const std::string& msg, Args&&... args) {
+    void debug(std::format_string<detail::log_format_arg_t<Args>...> fmt,
+        Args&&... args) {
         if (Level::DEBUG < detail::defaultLevel)
             return;
-        debug(std::vformat(msg, std::make_format_args(args...)));
+        debug(detail::render(fmt, std::forward<Args>(args)...));
     }
 
     template <typename... Args>
     requires (sizeof...(Args) != 0)
-    void trace(const std::string& msg, Args&&... args) {
+    void trace(std::format_string<detail::log_format_arg_t<Args>...> fmt,
+        Args&&... args) {
         if (Level::TRACE < detail::defaultLevel)
             return;
-        trace(std::vformat(msg, std::make_format_args(args...)));
+        trace(detail::render(fmt, std::forward<Args>(args)...));
     }
 
     /**
