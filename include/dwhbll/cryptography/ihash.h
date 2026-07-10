004e1640085f24694d40c22f220091f83c7c3d1d
teapot <teapot4195@gmail.com>
2026-07-09 23:56:42 -0400

    Hash Interface

---
+++ include/dwhbll/cryptography/ihash.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+#include <span>
+
+#include "banner.h"
+
+namespace dwhbll::cryptography {
+    class ihash {
+    public:
+        virtual ~ihash() = default;
+
+        virtual void update(const std::span<const std::uint8_t> &in) = 0;
+        virtual void finalize(std::span<std::uint8_t> output) = 0;
+        virtual void reset() = 0;
+
+        [[nodiscard]] virtual size_t digest_size() const = 0;
+        [[nodiscard]] virtual size_t block_size() const = 0;
+
+        void operator()(const std::span<const std::uint8_t> &in) {
+            update(in);
+        }
+    };
+}
