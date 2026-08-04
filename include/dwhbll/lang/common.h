fb6c641b13bd4b2b3e45b48332cfa8b31f89f499
teapot <teapot4195@gmail.com>
2026-08-03 23:11:53 -0400

    Add cursor head updates

--- include/dwhbll/lang/common.h
+++ include/dwhbll/lang/common.h
@@ -4,9 +4,20 @@
 
 namespace dwhbll::lang {
     struct cursor {
-        files::filejar::fileid file;
-        std::size_t line;
-        std::size_t column;
+        files::filejar::fileid file{};
+        std::size_t line{};
+        std::size_t column{};
+
+        cursor(files::filejar::fileid id) : file(id) {}
+
+        constexpr void next_line() {
+            line++;
+            column = 0;
+        }
+
+        constexpr void next_col() {
+            column++;
+        }
     };
 
     struct span {
