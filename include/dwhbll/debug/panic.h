e57ed4547b3035ff41da18003501fb84c654328c
mkryss <mkryss@proton.me>
2026-09-04 21:01:30 +0200

    make panic take string_view

--- include/dwhbll/debug/panic.h
+++ include/dwhbll/debug/panic.h
@@ -1,19 +1,18 @@
 #pragma once
 
 #include <format>
-#include <string>
 
 namespace dwhbll::debug {
 
 // I would add a skip parameter here to avoid printing internal stack
 // frames, but if I do that overload resolution shits itself
-[[noreturn]] void panic(const std::string& msg);
+[[noreturn]] void panic(std::string_view msg);
 
 [[noreturn]] void panic();
 
 template <typename... Args>
 requires (sizeof...(Args) != 0)
-[[noreturn]] void panic(const std::string& msg, Args&&... args) {
+[[noreturn]] void panic(std::string_view msg, Args&&... args) {
     panic(std::vformat(msg, std::make_format_args(args...)));
 }
 
