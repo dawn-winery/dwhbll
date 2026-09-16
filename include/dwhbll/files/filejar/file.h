dc1942f6e038fc5a04cdcf135a6fe09e16e042a8
teapot <teapot4195@gmail.com>
2026-09-16 12:41:15 -0400

    File hashing and other stuff

--- include/dwhbll/files/filejar/file.h
+++ include/dwhbll/files/filejar/file.h
@@ -2,35 +2,87 @@
 
 #include <filesystem>
 #include <vector>
+#include <dwhbll/debug/panic.h>
+#include <dwhbll/sanify/types.h>
+
+#ifdef _WIN32
+#error Windows builds not supported
+#endif
 
 namespace dwhbll::files::filejar {
-    class file {
+    struct file_metadata {
         std::filesystem::path _path;
-        std::vector<char> _contents;
+        std::filesystem::file_time_type last_write_time{};
+        std::size_t len{};
+        bool _exists{};
 
-        bool _exists;
+        mutable std::optional<std::uint64_t> hash{};
+
+        [[nodiscard]] constexpr bool probably_unchanged(const file_metadata& other) const {
+            if (!_exists || !other._exists)
+                return false;
+            return len == other.len &&
+                last_write_time == other.last_write_time;
+        }
+
+        [[nodiscard]] constexpr bool operator==(const file_metadata& other) const {
+            return probably_unchanged(other) && hash == other.hash;
+        }
+    };
+
+    class file {
+        file_metadata metadata;
+        int fd{-1};
+        sanify::u8* data{nullptr};
 
     public:
         explicit file(const std::filesystem::path &path);
 
+        ~file();
+
         file(const file &other) = delete;
 
-        file(file &&other) noexcept;
+        file(file &&other) noexcept
+            : metadata(std::move(other.metadata)),
+              fd(other.fd),
+              data(other.data) {
+            other.fd = -1;
+            other.data = nullptr;
+        }
 
         file & operator=(const file &other) = delete;
 
-        file & operator=(file &&other) noexcept;
+        file & operator=(file &&other) noexcept {
+            if (this == &other)
+                return *this;
+            metadata = std::move(other.metadata);
+            fd = other.fd;
+            data = other.data;
+            other.fd = -1;
+            other.data = nullptr;
+            return *this;
+        }
+
+        void compute_hash() const;
+
+        [[nodiscard]] bool file_unchanged_against(const file_metadata& other) const;
 
         [[nodiscard]] constexpr bool exists() const {
-            return _exists;
+            return metadata._exists;
         }
 
         [[nodiscard]] constexpr std::filesystem::path path() const {
-            return _path;
+            return metadata._path;
         }
 
-        [[nodiscard]] constexpr const std::vector<char>& contents() const {
-            return _contents;
+        [[nodiscard]] constexpr std::span<sanify::u8> contents() const {
+            if (!metadata._exists)
+                debug::panic("File doesn't exist!");
+            return {data, metadata.len};
+        }
+
+        [[nodiscard]] constexpr file_metadata get_metadata() const {
+            return metadata;
         }
     };
 }
--- src/dwhbll/files/filejar/file.cpp
+++ src/dwhbll/files/filejar/file.cpp
@@ -1,30 +1,61 @@
 #include <dwhbll/files/filejar/file.h>
 
+#include <cstring>
 #include <fstream>
 
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include <dwhbll/hash/xxh/xxh3_64.h>
+
 namespace dwhbll::files::filejar {
-    file::file(const std::filesystem::path &path) : _path(path),
-        _exists(std::filesystem::exists(path)) {
-        if (_exists) {
-            std::ifstream file(_path, std::ios::in);
-            _contents = std::vector<char>{
-                std::istreambuf_iterator<char>(file),
-                std::istreambuf_iterator<char>()
-            };
+    file::file(const std::filesystem::path &path) {
+        metadata = {canonical(path)};
+        metadata._exists = std::filesystem::exists(metadata._path);
+
+        if (metadata._exists) {
+            metadata.len = std::filesystem::file_size(metadata._path);
+            metadata.last_write_time = std::filesystem::last_write_time(metadata._path);
+
+            fd = open(metadata._path.c_str(), O_RDONLY);
+
+            if (fd < 0)
+                debug::panic("Attempted to open {}, got error: {}", metadata._path, strerror(errno));
+
+            data = (sanify::u8*)mmap(nullptr, metadata.len, PROT_READ, MAP_PRIVATE, fd, 0);
         }
     }
 
-    file::file(file &&other) noexcept: _path(std::move(other._path)),
-                                       _contents(std::move(other._contents)),
-                                       _exists(other._exists) {
+    file::~file() {
+        if (fd != -1) {
+            close(fd);
+            fd = -1;
+        }
+
+        if (data) {
+            munmap(data, metadata.len);
+            data = nullptr;
+        }
     }
 
-    file & file::operator=(file &&other) noexcept {
-        if (this == &other)
-            return *this;
-        _path = std::move(other._path);
-        _contents = std::move(other._contents);
-        _exists = other._exists;
-        return *this;
+    void file::compute_hash() const {
+        if (!metadata._exists || !data)
+            debug::panic("File doesn't exist!");
+
+        metadata.hash = hash::xxh::detail3_64::hash(data, metadata.len);
+    }
+
+    bool file::file_unchanged_against(const file_metadata &other) const {
+        if (metadata.probably_unchanged(other))
+            return true;
+
+        if (!metadata.hash.has_value())
+            compute_hash();
+
+        if (!other.hash.has_value())
+            debug::panic("Other metadata doesn't have has hvalue!");
+
+        return metadata.hash == other.hash;
     }
 }
