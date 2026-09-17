f03b357671ba5d6ace3207eca80cc41934c521e2
mkryss <mkryss@proton.me>
2026-09-17 17:54:17 +0200

    Port some more tests to new testing lib

--- tests/graphics/bitmap.cpp
+++ tests/graphics/bitmap.cpp
@@ -1,56 +1,61 @@
+#include <dwhbll/testing/testing.h>
+#include <dwhbll/graphics/bitmap/bitmap.h>
+
 #include <complex>
 #include <cstdint>
-#include <dwhbll/graphics/bitmap/bitmap.h>
-#include <functional>
+#include <filesystem>
 #include <print>
-#include <unordered_map>
 
-const int width = 4096;
-const int height = 2160;
-const int max_iters = 1000;
+using namespace dwhbll::test;
+
+namespace {
+
+const int width = 64;
+const int height = 64;
+const int max_iters = 20;
 
 int get_iters(double x, double y) {
-  std::complex<double> z = std::complex(0.0, 0.0);
-  std::complex<double> c = std::complex(x, y);
+    std::complex<double> z = std::complex(0.0, 0.0);
+    std::complex<double> c = std::complex(x, y);
 
-  for (int i = 0; i != max_iters; ++i) {
-    z = z * z + c;
-    if (std::abs(z) > 2)
-      return i;
-  }
-
-  return -1;
-}
-
-bool fractal_test() {
-  auto b = new dwhbll::graphics::bitmap::bitmap(4096, 2160);
-
-  for (int y = 0; y != height; ++y) {
-    for (int x = 0; x != width; ++x) {
-      double fx = (x - width / 2.0) * 2.0 / width;
-      double fy = (y - height / 2.0) * 2.0 / height;
-      int iters = get_iters(fx, fy);
-      uint8_t color = 256 * iters / max_iters;
-      (void)b->set_pixel(x, y, color, color, color);
+    for (int i = 0; i != max_iters; ++i) {
+        z = z * z + c;
+        if (std::abs(z) > 2)
+            return i;
     }
-  }
 
-  if (!b->write_to_file("output.bmp")) {
-    std::println("Failed to write output.bmp");
+    return -1;
+}
+
+} // namespace
+
+namespace graphics::bitmap {
+
+[[=test]]
+void fractal()
+{
+    auto b = new dwhbll::graphics::bitmap::bitmap(width, height);
+
+    for (int y = 0; y != height; ++y) {
+        for (int x = 0; x != width; ++x) {
+            double fx = (x - width / 2.0) * 2.0 / width;
+            double fy = (y - height / 2.0) * 2.0 / height;
+            int iters = get_iters(fx, fy);
+            uint8_t color = 256 * iters / max_iters;
+            (void)b->set_pixel(x, y, color, color, color);
+        }
+    }
+
+    if (!b->write_to_file("output.bmp")) {
+        std::println("Failed to write output.bmp");
+        delete b;
+        return;
+    }
+
     delete b;
-    return true;
-  }
-
-  delete b;
-  return false;
+    std::filesystem::remove("output.bmp");
 }
 
-std::unordered_map<std::string, std::function<bool()>> bitmap_dispatch{
-    {"fractal", fractal_test},
-};
-
-bool bitmap_test(std::optional<std::string> test_to_run) {
-  if (test_to_run.has_value() && bitmap_dispatch.contains(test_to_run.value()))
-    return bitmap_dispatch.at(test_to_run.value())();
-  return 1;
 }
+
+TEST_REGISTER_FILE();
--- tests/pool.cpp
+++ tests/pool.cpp
@@ -1,121 +1,130 @@
-#include <iostream>
-
+#include <dwhbll/testing/testing.h>
 #include <dwhbll/memory/pool.h>
-#include <optional>
+
+using namespace dwhbll::test;
 
 struct poolStruct {
     int f[1024];
 };
 
-bool pool_test(std::optional<std::string>) {
-    bool failed = false;
-    {
-        dwhbll::memory::Pool<poolStruct> testPool;
+namespace pool {
 
-        static_assert(dwhbll::memory::Pool<poolStruct>::block_size() > 0); // block size <= 0 results in infinite loop
+[[=test]]
+void pool_struct_allocate_free()
+{
+    dwhbll::memory::Pool<poolStruct> testPool;
 
-        auto before = testPool.used_size();
+    static_assert(dwhbll::memory::Pool<poolStruct>::block_size() > 0); // block size <= 0 results in infinite loop
 
-        for (int i = 0; i < 1024; i++)
-            auto _ = testPool.acquire();
+    auto before = testPool.used_size();
 
-        auto after = testPool.used_size();
+    for (int i = 0; i < 1024; i++)
+        auto _ = testPool.acquire();
 
-        if (before != after || after != 0) {
-            std::cerr << std::format("[ERR] (Pool<poolStruct>) Allocate then free failure: Before size: {} != After size: {}", before, after) << std::endl;
-            failed = true;
-        }
+    auto after = testPool.used_size();
 
-        before = testPool.used_size();
-
-        for (int i = 0; i < 1024; i++)
-            testPool.offer(testPool.acquire().disown()); // intentionally disown all the memory;
-
-        after = testPool.used_size();
-
-        if (before != after || after != 0) {
-            std::cerr << std::format("[ERR] (Pool<poolStruct>) Return memory failure: Before size: {} != After size: {}", before, after) << std::endl;
-            failed = true;
-        }
-
-        before = testPool.used_size();
-
-        for (int i = 0; i < 1024; i++)
-            testPool.acquire().disown(); // intentionally disown all the memory;
-
-        after = testPool.used_size();
-
-        if (before + 1024 != after) {
-            std::cerr << std::format("[ERR] (Pool<poolStruct>) Leak memory failure (memory not leaked): Before size: {} != After size: {}", before, after) << std::endl;
-            failed = true;
-        }
-    }
-
-    {
-        dwhbll::memory::Pool<int> testPool;
-
-        static_assert(dwhbll::memory::Pool<int>::block_size() > 0); // block size <= 0 results in infinite loop
-
-        auto before = testPool.used_size();
-
-        for (int i = 0; i < 1024; i++)
-            auto _ = testPool.acquire();
-
-        auto after = testPool.used_size();
-
-        if (before != after || after != 0) {
-            std::cerr << std::format("[ERR] (Pool<int>) Allocate then free failure: Before size: {} != After size: {}", before, after) << std::endl;
-            failed = true;
-        }
-
-        before = testPool.used_size();
-
-        for (int i = 0; i < 1024; i++)
-            testPool.offer(testPool.acquire().disown()); // intentionally disown all the memory;
-
-        after = testPool.used_size();
-
-        if (before != after || after != 0) {
-            std::cerr << std::format("[ERR] (Pool<int>) Return memory failure: Before size: {} != After size: {}", before, after) << std::endl;
-            failed = true;
-        }
-
-        before = testPool.used_size();
-
-        for (int i = 0; i < 1024; i++)
-            testPool.acquire().disown(); // intentionally disown all the memory;
-
-        after = testPool.used_size();
-
-        if (before + 1024 != after) {
-            std::cerr << std::format("[ERR] (Pool<int>) Leak memory failure (memory not leaked): Before size: {} != After size: {}", before, after) << std::endl;
-            failed = true;
-        }
-    }
-
-    {
-        dwhbll::memory::Pool<std::string> testPool;
-
-        auto* k = testPool.acquire("foo").disown();
-        auto* k2 = testPool.find("foo");
-
-        if (k != k2) {
-            std::cerr << "[ERR](Pool<std::string>) failure to correctly find string \"foo\" in pool" << std::endl;
-            return false;
-        }
-    }
-
-    {
-        dwhbll::memory::Pool<int> testPool;
-
-        auto* k = testPool.acquire(5).disown();
-        auto* k2 = testPool.find(5);
-
-        if (k != k2) {
-            std::cerr << "[ERR](Pool<int>) failure to correctly find value \"5\" in pool" << std::endl;
-            return false;
-        }
-    }
-
-    return failed;
+    EXPECT(before == after && after == 0);
 }
+
+[[=test]]
+void pool_struct_return_memory()
+{
+    dwhbll::memory::Pool<poolStruct> testPool;
+
+    auto before = testPool.used_size();
+
+    for (int i = 0; i < 1024; i++)
+        testPool.offer(testPool.acquire().disown()); // intentionally disown all the memory;
+
+    auto after = testPool.used_size();
+
+    EXPECT(before == after && after == 0);
+}
+
+[[=test]]
+void pool_struct_leak_memory()
+{
+    dwhbll::memory::Pool<poolStruct> testPool;
+
+    auto before = testPool.used_size();
+
+    for (int i = 0; i < 1024; i++)
+        testPool.acquire().disown(); // intentionally disown all the memory;
+
+    auto after = testPool.used_size();
+
+    EXPECT(before + 1024 == after);
+}
+
+[[=test]]
+void pool_int_allocate_free()
+{
+    dwhbll::memory::Pool<int> testPool;
+
+    static_assert(dwhbll::memory::Pool<int>::block_size() > 0); // block size <= 0 results in infinite loop
+
+    auto before = testPool.used_size();
+
+    for (int i = 0; i < 1024; i++)
+        auto _ = testPool.acquire();
+
+    auto after = testPool.used_size();
+
+    EXPECT(before == after && after == 0);
+}
+
+[[=test]]
+void pool_int_return_memory()
+{
+    dwhbll::memory::Pool<int> testPool;
+
+    auto before = testPool.used_size();
+
+    for (int i = 0; i < 1024; i++)
+        testPool.offer(testPool.acquire().disown()); // intentionally disown all the memory;
+
+    auto after = testPool.used_size();
+
+    EXPECT(before == after && after == 0);
+}
+
+[[=test]]
+void pool_int_leak_memory()
+{
+    dwhbll::memory::Pool<int> testPool;
+
+    auto before = testPool.used_size();
+
+    for (int i = 0; i < 1024; i++)
+        testPool.acquire().disown(); // intentionally disown all the memory;
+
+    auto after = testPool.used_size();
+
+    EXPECT(before + 1024 == after);
+}
+
+[[=test]]
+void pool_string_find()
+{
+    dwhbll::memory::Pool<std::string> testPool;
+
+    auto* k = testPool.acquire("foo").disown();
+    auto* k2 = testPool.find("foo");
+
+    REQUIRE(k == k2);
+}
+
+[[=test]]
+void pool_int_find()
+{
+    dwhbll::memory::Pool<int> testPool;
+
+    auto* k = testPool.acquire(5).disown();
+    auto* k2 = testPool.find(5);
+
+    REQUIRE(k == k2);
+}
+
+}
+
+TEST_REGISTER_FILE();
